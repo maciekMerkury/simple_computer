b0bb4136b1cdac98c115c898c4e19e2112dac11f
maciekMerkury <maciekmerkury2@gmail.com>
2021-10-29 17:02:52 +0200

    Started work on the assembler
    Features so far:
        - parsing lines into instructions, with rudamentary validation

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,3 +1,107 @@
 #[cfg(test)]
 mod tests;
 
+use crate::cpu::instruction::Instruction;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ParseError {
+    UnknownFirstToken(String),
+    NotEnoughArguments(String),
+    IncorrectArgument(String),
+    ArgumentParseError(String, String),
+}
+
+pub fn parse_line(line: &str) -> Result<Instruction, ParseError> {
+    // parse by spaces:
+    // TODO: moving away from ascii_whitespace could be a good idea, but this should work for now
+    let things: Vec<&str> = line.split_ascii_whitespace().into_iter().collect();
+    println!("parse_line things: {:?}", things);
+
+    // this is not the best things i've written in my life
+    match things[0] {
+        "add" => {
+            if things.len() < 4 {
+                return Err(ParseError::NotEnoughArguments(line.into()));
+            }
+            for i in 1..4 {
+                if things[i].len() != 2 {
+                    return Err(ParseError::IncorrectArgument(line.into()));
+                }
+            }
+
+            let mut regs: [usize; 3] = [0; 3];
+
+            for i in 1..4 {
+                match things[i][1..2].parse::<usize>() {
+                    Ok(ri) => {
+                        if ri > 6 {
+                            return Err(ParseError::ArgumentParseError(line.into(), things[i].into()));
+                        }
+                        regs[i - 1] = ri;
+                    },
+                    Err(e) => {return Err(ParseError::ArgumentParseError(line.into(), e.to_string()))},
+                }
+            }
+
+            return Ok(Instruction::Add(regs[0], regs[1], regs[2]));
+        },
+        "inv" => {
+            if things.len() < 2 {
+                return Err(ParseError::NotEnoughArguments(line.into()));
+            }
+            if things[1].len() != 2 {
+                return Err(ParseError::IncorrectArgument(line.into()));
+            }
+
+            match things[1][1..2].parse::<usize>() {
+                Ok(ri) => {
+                if ri > 6 {
+                    return Err(ParseError::ArgumentParseError(line.into(), things[1].into()));
+                }
+                    return Ok(Instruction::Inv(ri));
+                },
+                Err(e) => {return Err(ParseError::ArgumentParseError(line.into(), e.to_string()))},
+            }
+        },
+        "lod" => {
+            if things.len() < 4 {
+                return Err(ParseError::NotEnoughArguments(line.into()));
+            }
+            let mut regs: (u8, bool, usize) = Default::default();
+
+            match things[1].parse::<u8>() {
+                Ok(val) => {regs.0 = val},
+                Err(e) => {return Err(ParseError::ArgumentParseError(line.into(), e.to_string()))},
+            }
+            match things[2].parse::<bool>() {
+                Ok(val) => {regs.1 = val},
+                Err(e) => {return Err(ParseError::ArgumentParseError(line.into(), e.to_string()))},
+            }
+            match things[3][1..2].parse::<usize>() {
+                Ok(val) => {regs.2 = val},
+                Err(e) => {return Err(ParseError::ArgumentParseError(line.into(), e.to_string()))},
+            }
+
+            return Ok(Instruction::Lod(regs.0, regs.1, regs.2));
+        },
+        "jiz" => {
+            if things.len() < 3 {
+                return Err(ParseError::NotEnoughArguments(line.into()));
+            }
+            let mut regs: (usize, u8) = Default::default();
+
+            match things[1][1..2].parse::<usize>() {
+                Ok(val) => {regs.0 = val},
+                Err(e) => {return Err(ParseError::ArgumentParseError(line.into(), e.to_string()))},
+            }
+            match things[2].parse::<u8>() {
+                Ok(val) => {regs.1 = val},
+                Err(e) => {return Err(ParseError::ArgumentParseError(line.into(), e.to_string()))},
+            }
+
+            return Ok(Instruction::Jiz(regs.0, regs.1));
+        },
+        _ => {return Err(ParseError::UnknownFirstToken(line.into()))}
+    }
+}
+
--- src/assembler/tests.rs
+++ src/assembler/tests.rs
@@ -1,6 +1,35 @@
+use crate::cpu::instruction::Instruction::{self, *};
+use super::parse_line;
 
 #[test]
-fn works() {
-    assert!(true);
+fn parse_add() {
+    let instruction = "add r0 r1 r2";
+    let compiled = parse_line(instruction).unwrap();
+    assert!(compiled == Add(0, 1, 2));
+    assert!(parse_line("add r r2 r9").is_err());
+}
+
+#[test]
+fn parse_inv() {
+    let instruction = "inv r0";
+    let compiled = parse_line(instruction).unwrap();
+    assert!(compiled == Inv(0));
+    assert!(parse_line("inv r8").is_err());
+}
+
+#[test]
+fn parse_lod() {
+    let instruction = "lod 255 false r0";
+    let compiled = parse_line(instruction).unwrap();
+    assert!(compiled == Lod(255, false, 0));
+    assert!(parse_line("lod 400 fal r8").is_err());
+}
+
+#[test]
+fn parse_jiz() {
+    let instruction = "jiz r0 15";
+    let compiled = parse_line(instruction).unwrap();
+    assert!(compiled == Jiz(0, 15));
+    assert!(parse_line("jiz r0 256").is_err());
 }
 
